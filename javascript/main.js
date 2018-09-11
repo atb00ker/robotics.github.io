0da29d5733a5b03e9de79e505dce10552c6a7e19
arpit135 <40313365+arpit135@users.noreply.github.com>
2018-09-11 23:03:37 +0530

    Add files via upload
    added website structure and navbar.

---
+++ javascript/main.js
@@ -0,0 +1,34 @@
+(function () {
+    var navLinks = $('nav ul li a'),
+        navH = $('nav').height(),
+        section = $('section'),
+        documentEl = $(document);
+
+    documentEl.on('scroll', function () {
+        var currentScrollPos = documentEl.scrollTop();
+
+        section.each(function () {
+            var self = $(this);
+            if (self.offset().top < (currentScrollPos + navH) && (currentScrollPos + navH) < (self.offset().top + self.outerHeight())) {
+                var targetClass = '.' + self.attr('class') + '-marker';
+                navLinks.removeClass('active');
+                $(targetClass).addClass('active');
+            }
+        });
+
+    });
+
+
+
+})();
+
+function myFunction(x) {
+    x.classList.toggle("change");
+
+
+}
+$(document).ready(function () {
+    $("#menuicon").click(function () {
+        $("#navbar").toggle();
+    });
+});
